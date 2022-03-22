4dfc7cdd13199ece7d3a9ab06fd859bdb0040a17
juumelisa <jumelisa09@gmail.com>
2022-03-22 23:19:30 +0700

    category product

---
+++ src/controllers/productCategory.js
@@ -0,0 +1,107 @@
+const responseHandler = require('../helpers/responseHandler');
+const ProductCategory = require('../models/productCategory');
+const Product = require('../models/product');
+const Category = require('../models/category');
+
+const { APP_URL } = process.env;
+
+exports.getAllProductCategory = async (req, res) => {
+  let { limit, page } = req.query;
+  limit = parseInt(limit, 10) || 5;
+  page = parseInt(page, 10) || 1;
+  const url = `${APP_URL}/product?`;
+  const offset = (page - 1) * limit;
+  const results = await ProductCategory.findAll({
+    where: {
+      is_deleted: 0,
+    },
+    limit,
+    offset,
+  });
+  const count = await ProductCategory.count();
+  const last = Math.ceil(count / limit);
+  const pageInfo = {
+    prev: page > 1 ? `${url}page=${page - 1}&limit=${limit}` : null,
+    next: page < last ? `${url}page=${page + 1}&limit=${limit}` : null,
+    totalData: count,
+    currentPage: page,
+    lastPage: last,
+  };
+  return responseHandler(res, 200, 'List of product Categories', results, pageInfo);
+};
+
+exports.createProductCategory = async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.body.id_product);
+    const category = await Category.findByPk(req.body.id_category);
+    if (!product || product.dataValues.is_deleted) {
+      return responseHandler(res, 400, 'Product not found', null, null);
+    }
+    if (!category) {
+      return responseHandler(res, 400, 'Category not found', null, null);
+    }
+    const checkProductCategory = await ProductCategory.findAll({
+      where: {
+        id_product: req.body.id_product,
+        id_category: req.body.id_category,
+      },
+    });
+    if (checkProductCategory) {
+      return responseHandler(res, 400, 'Product category already exist', null, null);
+    }
+    const productCategory = await ProductCategory.create(req.body);
+    return responseHandler(res, 200, 'Product category created', productCategory);
+  } catch (e) {
+    return responseHandler(res, 400, 'Can\'t create product category', e, null);
+  }
+};
+
+// exports.productDetail = async (req, res) => {
+//   const { id } = req.params;
+//   const product = await Product.findByPk(id);
+//   if (product && product.is_deleted === false) {
+//     return responseHandler(res, 200, 'Product detail', product, null);
+//   }
+//   return responseHandler(res, 404, 'Product not found', null, null);
+// };
+
+exports.updateProductCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (req.body.id_product) {
+      const product = await Product.findByPk(req.body.id_product); // check product
+      if (!product || product.dataValues.is_deleted) {
+        return responseHandler(res, 400, 'Product not found', null, null);
+      }
+    }
+    if (req.body.id_category) {
+      const category = await Category.findByPk(req.body.id_category); // check category
+      if (!category) {
+        return responseHandler(res, 400, 'Category not found', null, null);
+      }
+    }
+    const productCategory = await ProductCategory.findByPk(id); // check product category
+    if (!productCategory || productCategory.dataValues.is_deleted) {
+      return responseHandler(res, 404, 'Data not found', null, null);
+    }
+    Object.keys(req.body).forEach((data) => {
+      productCategory[data] = req.body[data];
+    });
+    await productCategory.save();
+    return responseHandler(res, 200, 'Product updated', productCategory, null);
+  } catch (e) {
+    // const errMessage = e.errors.map((err) => ({ field: err.path, message: err.message }));
+    return responseHandler(res, 400, 'Can\'t update product', e, null);
+  }
+};
+
+exports.deleteProductCategory = async (req, res) => {
+  const { id } = req.params;
+  const productCategory = await ProductCategory.findByPk(id);
+  if (!productCategory || productCategory.dataValues.is_deleted) {
+    return responseHandler(res, 404, 'Product category not found', null, null);
+  }
+  productCategory.is_deleted = 1;
+  await productCategory.save();
+  return responseHandler(res, 200, 'Product category was deleted', null, null);
+};
---
+++ src/models/productCategory.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../helpers/sequelize');
+
+const Product = require('./product');
+const Category = require('./category');
+
+const ProductCategory = sequelize.define('product_category', {
+  id_product: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Product,
+      key: 'id',
+    },
+  },
+  id_category: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Category,
+      key: 'id',
+    },
+  },
+  is_deleted: {
+    type: Sequelize.BOOLEAN,
+  },
+});
+
+module.exports = ProductCategory;
