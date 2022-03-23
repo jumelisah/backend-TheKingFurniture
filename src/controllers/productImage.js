fea1fe31dbcabcb7274dfbc0752d56a9ff67652f
juumelisa <jumelisa09@gmail.com>
2022-03-23 15:29:22 +0700

    feat: delete image

--- src/controllers/productImage.js
+++ src/controllers/productImage.js
@@ -1,6 +1,9 @@
 const responseHandler = require('../helpers/responseHandler');
 const ProductImage = require('../models/productImage');
 const Product = require('../models/product');
+const { deleteFile } = require('../helpers/fileHandler');
+const { cloudPathToFileName } = require('../helpers/converter');
+const { deleteImages } = require('../helpers/deleteArrayImages');
 
 exports.getAllProductImages = async (req, res) => {
   const images = await ProductImage.findAll();
@@ -40,6 +43,7 @@ exports.addImage = async (req, res) => {
       },
     });
     if (productImages.length > 9) {
+      deleteImages(images);
       return responseHandler(res, 400, 'Maximum images allow: 10 images', null, null);
     }
     const data = { id_product: req.body.id_product };
@@ -48,6 +52,7 @@ exports.addImage = async (req, res) => {
       data.image = pic.path;
       const productImage = await ProductImage.create(data);
       if (!productImage) {
+        deleteImages(images);
         return responseHandler(res, 400, 'Cant upload image', null, null);
       }
       responses[idx] = productImage;
@@ -55,40 +60,56 @@ exports.addImage = async (req, res) => {
     });
     return responseHandler(res, 200, 'Success', responses, null);
   } catch (e) {
+    if (req.files) {
+      deleteImages(req.files);
+    }
     return responseHandler(res, 400, 'Can\'t create product category', e, null);
   }
 };
 
 exports.updateProductImage = async (req, res) => {
-  const { id } = req.params;
-  const data = { id };
-  if (req.files) {
-    data.image = req.files[0].path;
-  }
-  if (req.body.id_product) {
-    data.id_product = req.body.id_product;
-    const product = await Product.findByPk(data.id_product);
-    if (!product || product.dataValues.is_deleted) {
-      return responseHandler(res, 404, 'Product not found', null, null);
+  try {
+    const { id } = req.params;
+    const data = { id };
+    if (req.files) {
+      data.image = req.files[0].path;
     }
+    if (req.body.id_product) {
+      data.id_product = req.body.id_product;
+      const product = await Product.findByPk(data.id_product);
+      if (!product || product.dataValues.is_deleted) {
+        if (req.files) {
+          deleteFile(cloudPathToFileName(data.image));
+        }
+        return responseHandler(res, 404, 'Product not found', null, null);
+      }
+    }
+    const productImages = await ProductImage.findAll({
+      where: {
+        id_product: req.body.id_product,
+      },
+    });
+    if (productImages.length > 9) {
+      return responseHandler(res, 400, 'Maximum images allow: 10 images', null, null);
+    }
+    const productImage = await ProductImage.findByPk(id);
+    if (!productImage) {
+      if (req.files) {
+        deleteFile(cloudPathToFileName(data.image));
+      }
+      return responseHandler(res, 404, 'Data not found', null, null);
+    }
+    Object.keys(data).forEach((x) => {
+      productImage[x] = data[x];
+    });
+    await productImage.save();
+    return responseHandler(res, 200, 'Updated image', productImage, null, null);
+  } catch (e) {
+    if (req.files) {
+      deleteFile(cloudPathToFileName(req.files.path));
+    }
+    return responseHandler(res, 400, 'Error', e, null);
   }
-  const productImages = await ProductImage.findAll({
-    where: {
-      id_product: req.body.id_product,
-    },
-  });
-  if (productImages.length > 9) {
-    return responseHandler(res, 400, 'Maximum images allow: 10 images', null, null);
-  }
-  const productImage = await ProductImage.findByPk(id);
-  if (!productImage) {
-    return responseHandler(res, 404, 'Data not found', null, null);
-  }
-  Object.keys(data).forEach((x) => {
-    productImage[x] = data[x];
-  });
-  await productImage.save();
-  return responseHandler(res, 200, 'Updated image', productImage, null, null);
 };
 
 exports.deleteProductImage = async (req, res) => {
@@ -99,6 +120,7 @@ exports.deleteProductImage = async (req, res) => {
       return responseHandler(res, 404, 'Data not found', null, null);
     }
     await productImage.destroy();
+    deleteFile(cloudPathToFileName(productImage.dataValues.image));
     return responseHandler(res, 200, 'Data was successfully deleted');
   } catch (e) {
     return responseHandler(res, 500, 'Unexpected error', e, null);
