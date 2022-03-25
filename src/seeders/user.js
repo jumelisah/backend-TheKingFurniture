a828452cfb75f85b78383f121f625496accb369d
7shiroi <lucky7kelvin@gmail.com>
2022-03-25 18:35:32 +0800

    feat: upload image user

--- src/seeders/user.js
+++ src/seeders/user.js
@@ -3,7 +3,9 @@ const User = require('../models/user');
 const users = async () => {
   const user = await User.findAll();
 
-  const seedUser = { name: 'Admin', email: 'admin@mail.com', password: 'Admin123' };
+  const seedUser = {
+    name: 'Admin', email: 'admin@mail.com', password: 'Admin123', role: 1,
+  };
 
   if (user.length === 0) {
     await User.create(seedUser);
