9322ad9c1c8de8ac9eea760669e73a69c86e81a6
juumelisa <jumelisa09@gmail.com>
2022-05-23 16:50:42 +0700

    feat: product category

--- src/models/productCategory.js
+++ src/models/productCategory.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../helpers/sequelize');
 
-// const Product = require('./product');
+const Product = require('./product');
 const Category = require('./category');
 
 const ProductCategory = sequelize.define('product_category', {
   id_product: {
     type: Sequelize.INTEGER,
-    // references: {
-    //   model: Product,
-    //   key: 'id',
-    // },
+    references: {
+      model: Product,
+      key: 'id',
+    },
   },
   id_category: {
     type: Sequelize.INTEGER,
