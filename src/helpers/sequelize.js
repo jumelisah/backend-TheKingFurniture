845e4e384183b0e59790f08d9778569966c9495b
juumelisa <jumelisa09@gmail.com>
2022-05-23 13:16:47 +0700

    feat: pg

--- src/helpers/sequelize.js
+++ src/helpers/sequelize.js
@@ -7,6 +7,12 @@ const {
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
 });
 
 module.exports = sequelize;
